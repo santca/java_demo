e4465ed77781089fb5c7127de10a2efdfe96295e
fishcy521 <fishcy521@163.com>
2016-09-08 16:44:48 +0800

    循环初步

---
+++ demo1/src/main/java/Test6.java
@@ -0,0 +1,24 @@
+/**
+ * Created by chenyan on 2016/9/8.
+ */
+public class Test6 {
+    public static void main(String[] args) {
+       /* for (int index =1;index < 101; index++) {
+            System.out.println("第"+index+"遍:好好学习，天天向上");
+        }*/
+
+        /*int index = 1;
+        while (index<10) {
+
+            System.out.println("哈哈");
+            index ++;
+        }*/
+        int index = 1;
+        do {
+            System.out.println("hehe");
+            index ++;
+        }
+        while(index<5);
+
+    }
+}
