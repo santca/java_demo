d0d29b2f8cc87d7a673c7f1ede06a32d846d0155
fishcy521 <fishcy521@163.com>
2016-09-11 16:45:12 +0800

    求任意一个数的位数的程序 test13

---
+++ demo1/src/main/java/Test13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by chenyan on 2016/9/11.
+ */
+public class Test13 {
+
+    public static void main(String[] args) {
+
+        System.out.print("请输入一个数字:");
+        int input_number = new Scanner(System.in).nextInt();
+
+        int number_times = 0;
+        int index=0;
+        while(true) {
+            index ++;
+            number_times = input_number/10;
+            if (number_times==0) {
+                break;
+            }
+            input_number = number_times;
+        }
+
+        System.out.println("这是一个"+index+"位数");
+    }
+}
